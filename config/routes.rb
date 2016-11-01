1ad7b265dbb1bd8ae6bdb7f2e5737cfa3c81faa9
laurenfb <laurenfriesbrink@gmail.com>
2016-10-31 17:11:41 -0700

    homepages controller & apply foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'homepages#index'
+
+  get 'homepages/search'
+
+  get 'homepages/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
