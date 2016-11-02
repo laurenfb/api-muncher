4e7db025a6c3764344e12c80cc994c365d17b339
laurenfb <laurenfriesbrink@gmail.com>
2016-11-02 11:07:47 -0700

    tests tests tests. all the tests. refactoring because while writing the code it became clear the two methods did different things.

--- test/lib/api_wrapper_test.rb
+++ test/lib/api_wrapper_test.rb
@@ -8,65 +8,67 @@ class APIWrapperTest < ActionController::TestCase
   end
 
 ############# token & permissions stuff #############
-  test 'returns an html page when app id is wrong' do
-    skip
-    VCR.use_cassette("bad-app-id") do
-      recipe_list = APIWrapper.search('cat', 'bad-app-id', ENV["APP_KEY"])
+  # test 'returns an html page when app id is wrong' do
+  #   # skip
+  #   VCR.use_cassette("bad-app-id") do
+  #     response = APIWrapper.search('cat', '232354353', ENV["APP_KEY"])
+  #
+  #     first_14_char = response[0..14]
+  #
+  #     assert_equal(first_14_char, "<!DOCTYPE html>")
+  #   end
+  # end
+  #
+  # test 'returns an html page when app key is wrong' do
+  #   skip
+  #   VCR.use_cassette("bad-app-key") do
+  #     response = APIWrapper.search('cat', ENV["APP_ID"], 'bad-app-key')
+  #
+  #     assert_equal(response['headers']['Content-Type'], "text/html")
+  #   end
+  # end
+  #
+  # test 'works fine when app key is missing' do
+  #   skip
+  #   VCR.use_cassette("missing-app-key") do
+  #     response = APIWrapper.search('cat', ENV["APP_ID"], nil)
+  #     # puts response
+  #     # assert_not_nil(response)
+  #   end
+  # end
+  #
+  # test 'works fine when app id is missing' do
+  #   skip
+  #   VCR.use_cassette("missing-app-id") do
+  #     response = APIWrapper.search('cat', nil, ENV["APP_KEY"])
+  #
+  #     assert_not_empty(response)
+  #   end
+  # end
+  #
+  # test 'works fine when app id & app key are missing' do
+  #   skip
+  #   VCR.use_cassette("missing-app-stuff") do
+  #     response = APIWrapper.search('cat', nil, nil)
+  #
+  #     assert_not_empty(response)
+  #   end
+  # end
 
-      assert_equal(recipe_list['headers']['Content-Type'], "text/html")
-    end
-  end
-
-  test 'returns an html page when app key is wrong' do
-    skip
-    VCR.use_cassette("bad-app-key") do
-      recipe_list = APIWrapper.search('cat', ENV["APP_ID"], 'bad-app-key')
-
-      assert_equal(recipe_list['headers']['Content-Type'], "text/html")
-    end
-  end
-
-  test 'works fine when app key is missing' do
-    skip
-    VCR.use_cassette("missing-app-key") do
-      recipe_list = APIWrapper.search('cat', ENV["APP_ID"], nil)
-
-      assert_not_empty(recipe_list)
-    end
-  end
-
-  test 'works fine when app id is missing' do
-    skip
-    VCR.use_cassette("missing-app-id") do
-      recipe_list = APIWrapper.search('cat', nil, ENV["APP_KEY"])
-
-      assert_not_empty(recipe_list)
-    end
-  end
-
-  test 'works fine when app id & app key are missing' do
-    skip
-    VCR.use_cassette("missing-app-stuff") do
-      recipe_list = APIWrapper.search('cat', nil, nil)
-
-      assert_not_empty(recipe_list)
-    end
-  end
-
-######## search method #############
+######## make_recipe_list  method #############
   test 'search returns a non-empty array if a reasonable term is searched' do
-    skip
+    # skip
     VCR.use_cassette('chicken') do
-      recipe_list = APIWrapper.search('chicken')
+      recipe_list = APIWrapper.make_recipe_list(APIWrapper.search('chicken'))
       assert(recipe_list.is_a?(Array))
       assert_not_empty(recipe_list)
     end
   end
 
   test 'search returns an array of Recipe objects' do
-    skip
+    # skip
     VCR.use_cassette('chicken') do
-      recipe_list = APIWrapper.search('chicken')
+      recipe_list = APIWrapper.make_recipe_list(APIWrapper.search('chicken'))
 
       recipe_list.each do |recipe|
         assert(recipe.is_a?(Recipe))
@@ -75,18 +77,40 @@ class APIWrapperTest < ActionController::TestCase
   end
 
   test 'search returns the correct length of array (this based on an API call to Postman)' do
-    skip
+    # skip
     VCR.use_cassette('cat') do
-      recipe_list = APIWrapper.search('cat')
-      assert_equal(recipe_list.length, 172)
+      response = APIWrapper.search('cat')
+      recipe_list = APIWrapper.make_recipe_list(response)
+      assert_equal(response['count'], 172)
+      assert_equal(recipe_list.length, 10)
     end
   end
 
-  test 'blank search returns array with nothing in it' do
-    skip
+  test 'blank search, made to recipes, returns array with nothing in it' do
+    # skip
     VCR.use_cassette('nil-search') do
-      recipe_list = APIWrapper.search(nil)
+      recipe_list = APIWrapper.make_recipe_list(APIWrapper.search(nil))
       assert_empty(recipe_list)
+    end
+  end
+
+  test 'when counts are added, the length of the full list of hits does not change' do
+    VCR.use_cassette('lots-of-results') do
+      first_page = APIWrapper.search('cat')
+      second_page = APIWrapper.search('cat', 11, 20)
+      assert_equal(first_page['count'], second_page['count'], 172)
+    end
+  end
+
+  test 'when counts are added, the results returned are not the same as when the counts are not added' do
+    VCR.use_cassette('more-results') do
+      first_page = APIWrapper.make_recipe_list( APIWrapper.search('cat'))
+      second_page = APIWrapper.make_recipe_list(APIWrapper.search('cat', 11, 20))
+      first_page.each do |recipe1|
+        second_page.each do |recipe2|
+          assert_not_equal(recipe1, recipe2)
+        end
+      end
     end
   end
 
