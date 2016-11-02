e85547b1239bd5a6da817dec063df7ac6148ce54
laurenfb <laurenfriesbrink@gmail.com>
2016-11-02 11:08:04 -0700

    APIWrapper.make_recipe_list method

--- lib/api_wrapper.rb
+++ lib/api_wrapper.rb
@@ -1,4 +1,33 @@
 require 'httparty'
+require 'uri'
 
 class APIWrapper
+  BASE_URL = "https://api.edamam.com/search?"
+  APP_ID = ENV["APP_ID"]
+  APP_KEY = ENV["APP_KEY"]
+
+  def self.search(query, from = 0, to = 10, id = APP_ID, key = APP_KEY)
+    # if there are spaces, pull them out. i know this looks kinda ugly but escape method is annoying for "" and nil.
+    if !query.nil?
+      query = URI.escape(query)
+    end
+
+    url = BASE_URL + "&app_id=#{id}" + "&app_key=#{key}" + "&q=#{query}" + "&from=#{from}" + "&to=#{to}"
+
+
+    return HTTParty.get(url)
+  end
+
+  def self.make_recipe_list(response)
+    recipe_list = []
+    if response['count']
+      response['hits'].each do |hash|
+        recipe = Recipe.new(hash)
+        recipe_list << recipe
+      end
+    end
+    return recipe_list
+  end
+
+
 end
