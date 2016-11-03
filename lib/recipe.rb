a3c5e8897951dfc40210d03cc491610cfe222c07
laurenfb <laurenfriesbrink@gmail.com>
2016-11-03 14:22:51 -0700

    correct info on show page, though it does not look pretty

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,7 +1,7 @@
 require 'uri'
 
 class Recipe
-  attr_reader :id, :label, :image, :url, :yield, :diet_labels, :health_labels, :ingredient_list
+  attr_reader :id, :label, :image, :url, :yield, :diet_labels, :health_labels, :ingredient_list, :source
 
   def initialize(params)
     if params.empty? # hits array & uri arry will be empty, whether the parameter is from the recipe query or a URI lookup
@@ -18,11 +18,13 @@ class Recipe
       @id = URI.encode(params[0]['uri'].to_s)
       @label = params[0]['label'].to_s
       @image = params[0]['image']
+      @source = params[0]['source']
       @url = params[0]['url']
       @yield = params[0]['yield']
       @diet_labels = params[0]['dietLabels']
       @health_labels = params[0]['healthLabels']
       @ingredient_list = params[0]['ingredientLines']
+
     end
 
 
