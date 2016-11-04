baabc350210ab3b44c042e84fe1a71a1bd46c10d
laurenfb <laurenfriesbrink@gmail.com>
2016-11-03 21:00:38 -0700

    the next and back buttons are there, and they ALMOST work

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   get 'results' => 'homepages#index', as: 'results'
 
   get '/show/:label' => 'homepages#show', as: 'show_recipe'
+
+  get 'back' => 'homepages#index', as: 'back'
+
+  get 'next' => 'homepages#index', as: 'next'
 end
